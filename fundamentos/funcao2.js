63c194d8f63c602d819fcfc5132bbacbffab3792
Lino Veloso <linoveloso@outlook.com>
2018-03-20 17:05:08 -0300

    estudos
    estudos

---
+++ fundamentos/funcao2.js
@@ -0,0 +1,22 @@
+// Armazenando uma funcao em uma variavel
+
+const imprimirSoma = function (a, b) {
+    console.log(a + b)
+}
+
+imprimirSoma(2, 3)
+
+//Armazenando uma funcao arrow em uma variavel
+
+const soma = (a, b) => {
+    return a + b
+}
+console.log(soma(2, 3))
+
+//Retorno Implícito
+const subtracao = (a, b) => a - b
+console.log(subtracao(2, 3))
+
+const imprirmir2 = a => console.log(a)
+
+imprirmir2('LEGAL!!!!!')
