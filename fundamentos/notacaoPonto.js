961d0c3e12c1e5481aff32a0e13e845741f90918
Lino Veloso <linoveloso@outlook.com>
2019-01-10 17:58:26 -0200

    Notação Ponto update

--- fundamentos/notacaoPonto.js
+++ fundamentos/notacaoPonto.js
@@ -4,3 +4,29 @@ console.log(typeof console);
 console.log(Math.ceil(6.1));
 //arredonda para o próximo inteiro
 console.log(Math.round(6.1));
+
+const obj1 = {}
+
+obj1.nome = 'Bola'
+//obj1['nome'] = 'Quadrado'
+
+console.log(obj1.nome);
+
+function Obj(nome){
+    this.nome = nome
+    this.exec = function() {
+        console.log('Exec...')     
+    }
+}
+
+const obj2 = new Obj('cadeira')
+const obj3 = new Obj('mesa')
+
+console.log(obj2.nome)
+console.log(obj3.nome)
+
+obj3.exec()
+
+
+
+
