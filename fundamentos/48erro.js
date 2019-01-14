9c99ec1fe0402d79e7649be35fe4daf58c3ad17f
Lino Veloso <linoveloso@outlook.com>
2019-01-14 19:07:36 -0200

    Update 48erro.js

--- fundamentos/48erro.js
+++ fundamentos/48erro.js
@@ -1,6 +1,27 @@
-const obj = {nome: 'Lino'}
-function imprimirNomeGritado(obj) {
-    console.log(obj.name.toUpperCase() + '!!!')
+function tratarErroELancar(erro) {
+    //throw new Error('This is a fckng error')
+    throw 'This is a fcking error'
+    /* throw {
+        nome: erro.name,
+        msg: erro.message,
+        date: new Date
+     
+    } 
+    */
+
 }
 
+function imprimirNomeGritado(obj) {
+    try {
+        console.log(obj.name.toUpperCase() + '!!!')
+    } catch (error) {
+        tratarErroELancar(error)
+    }
+    finally {
+        console.log('Acabou, FIM');
+    }
+
+}
+const obj = { name: 'Lino' }
 imprimirNomeGritado(obj)
+
