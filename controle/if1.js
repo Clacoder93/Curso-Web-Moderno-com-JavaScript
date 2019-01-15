0f370f8d89ddf9b61e2ff3c3d6d6885565d462d0
Lino Veloso <linoveloso@outlook.com>
2019-01-15 10:34:16 -0200

    Update if1.js

--- controle/if1.js
+++ controle/if1.js
@@ -5,3 +5,23 @@ function soBoaNoticia(nota) {
 }
 soBoaNoticia(8.1)
 soBoaNoticia(Math.ceil(6.1))
+soBoaNoticia(6.3)
+
+function seForVerdadeEuFalo(valor) {
+    if(valor)
+        console.log('É Verdade...'+valor)
+        
+}
+seForVerdadeEuFalo()
+seForVerdadeEuFalo(null)
+seForVerdadeEuFalo(undefined)
+seForVerdadeEuFalo(NaN)
+seForVerdadeEuFalo('')
+seForVerdadeEuFalo(0)
+seForVerdadeEuFalo(-1)
+seForVerdadeEuFalo(' ')
+seForVerdadeEuFalo(7<3)
+seForVerdadeEuFalo(7>3)
+seForVerdadeEuFalo('?')
+seForVerdadeEuFalo({})
+seForVerdadeEuFalo([])
