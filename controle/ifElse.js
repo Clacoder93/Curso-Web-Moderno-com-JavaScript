4d69b6eb87efd57f2394145bbf1148085ec318a6
Lino Veloso <linoveloso@outlook.com>
2019-01-15 11:00:20 -0200

    Create ifElse.js

---
+++ controle/ifElse.js
@@ -0,0 +1,26 @@
+
+const imprimirResultado = function (nota) {
+    if (nota >= 7) {
+        console.log('Parabéns, você foi aprovado')
+    }
+    else {
+        console.log('Infelizmente você foi reprovado')
+    }
+}
+
+imprimirResultado(10)
+imprimirResultado(4)
+
+/*
+function imprimirResultado(nota) {
+    if (nota >= 7) {
+        console.log('Parabéns, você foi aprovado')
+    }
+    else {
+        console.log('Infelizmente você foi reprovado')
+    }
+}
+
+imprimirResultado(10)
+imprimirResultado(4)
+*/
