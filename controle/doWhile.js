db0842d7c19fe1a7964cff06f9d46e7f0b45332c
Lino Veloso <linoveloso@outlook.com>
2019-01-17 17:09:54 -0200

    Update doWhile.js

--- controle/doWhile.js
+++ controle/doWhile.js
@@ -3,11 +3,12 @@ function getInteiroAleatorioEntre(min, max) {
     return Math.floor(valor)
 }
 
-let opcao = 0
-
-while (opcao!=-1) {
-    opcao = getInteiroAleatorioEntre(-1, 10)
+let opcao = -1
+do {
+    opcao = getInteiroAleatorioEntre(-1, 1000)
     console.log(`Opção escolhida foi ${opcao}`);
-}
+    
+} while (opcao!=-1);
+
 
 console.log('Até a proxima');
