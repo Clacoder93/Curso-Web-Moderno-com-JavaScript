15ddb9136856788983966af112b92ec95683d246
Lino Veloso <linoveloso@outlook.com>
2019-01-17 17:24:42 -0200

    Create for1.js

---
+++ controle/for1.js
@@ -0,0 +1,13 @@
+let contador = 1
+
+while (contador <= 10) {
+    console.log(`Valor do contador é ${contador}`);
+   contador++
+}
+
+
+for (let i = 1; i <= 10; i++) {
+    console.log(`i = ${i}`);
+    
+    
+}
