10ace8e210afb3310fbcfd34123ae98df06731d7
Lino Veloso <linoveloso@outlook.com>
2019-01-18 10:24:17 -0200

    Update for1.js

--- controle/for1.js
+++ controle/for1.js
@@ -1,13 +1,11 @@
 let contador = 1
 
 while (contador <= 10) {
-    console.log(`Valor do contador é ${contador}`);
-   contador++
+    console.log(`Valor do contador é ${contador}`)
+    contador++
 }
 
 
 for (let i = 1; i <= 10; i++) {
-    console.log(`i = ${i}`);
-    
-    
+    console.log(`i = ${i}`)
 }
